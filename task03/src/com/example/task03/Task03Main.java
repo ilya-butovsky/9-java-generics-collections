2863697610eb63c922a690a56c039d502d812b83
Ilya B <ilya@ml1000.net>
2020-12-18 14:00:45 +0500

    task03 [butovsky]

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,9 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Task03Main {
 
@@ -19,6 +17,30 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, charset));
+        HashMap<Integer, Set<String>> hashMap = new HashMap<>();
+        bufferedReader.lines().forEach(line -> {
+            if (line.length() >= 3) {
+                Stream<Character.UnicodeBlock> unicodeBlockStream = line.chars().mapToObj(Character.UnicodeBlock::of);
+                if (unicodeBlockStream.allMatch(s -> s.equals(Character.UnicodeBlock.CYRILLIC))) {
+                    Set<String> set = hashMap.computeIfAbsent(line.length(), key -> new HashSet<>());
+                    set.add(line.toLowerCase());
+                }
+            }
+        });
+        HashSet<Set<String>> result = new HashSet<>();
+        hashMap.forEach((key, set) -> set.forEach(string -> {
+            TreeSet<String> inter_res = new TreeSet<>();
+            int[] string_chars = string.chars().sorted().toArray();
+            set.forEach(another -> {
+                if (Arrays.equals(string_chars, another.chars().sorted().toArray())) {
+                    inter_res.add(another);
+                }
+            });
+            if (inter_res.size() >= 2) result.add(inter_res);
+        }));
+        ArrayList<Set<String>> resultList = new ArrayList<>(result);
+        resultList.sort(Comparator.comparing(s -> s.iterator().next()));
+        return resultList;
     }
 }
