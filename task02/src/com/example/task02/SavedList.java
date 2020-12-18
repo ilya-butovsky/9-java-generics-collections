e3cffa5a696aea7ff7dcc6e458754c3ea073acfe
Ilya B <ilya@ml1000.net>
2020-12-18 13:42:01 +0500

    task02 [butovsky]

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,69 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
 
+    private final ArrayList<E> list;
+    private final Path path;
+
     public SavedList(File file) {
+        Objects.requireNonNull(file);
+        ArrayList<E> tmp;
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))) {
+            tmp = (ArrayList<E>) objectInputStream.readObject();
+            if (tmp == null) {
+                tmp = new ArrayList<>();
+            }
+        } catch (Exception exception) {
+            System.err.println(exception.toString() + ". Creating new empty list...");
+            tmp = new ArrayList<>();
+        }
+        this.list = tmp;
+        this.path = file.toPath();
+
+    }
+
+    private void sync() {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(Files.newOutputStream(this.path))) {
+            outputStream.writeObject(this.list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return this.list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E elem = this.list.set(index, element);
+        this.sync();
+        return elem;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        this.list.add(index, element);
+        this.sync();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E elem = this.list.remove(index);
+        this.sync();
+        return elem;
     }
 }
